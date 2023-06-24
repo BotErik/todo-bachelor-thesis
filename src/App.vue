96d9a3ed43f80785586b43141f6504ac627bea04
BotErik <43347810+BotErik@users.noreply.github.com>
2023-06-24 13:19:13 +0200

    them toggle layout fix

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,7 @@ export default {
 
 <template>
   <div class="relative z-0 p-[30px] w-screen h-screen bg-light-background dark:bg-dark-background">
-    <button @click="toggleDark()" id="modeToggle" class="fixed top-[30px] right-[30px] p-[2px] rounded-full h-[71px] w-[37px] bg-light-text dark:bg-dark-text">
+    <button @click="toggleDark()" id="modeToggle" class="fixed z-[50] top-[30px] right-[30px] p-[2px] rounded-full h-[71px] w-[37px] bg-light-text dark:bg-dark-text">
       <div class="absolute bottom-[2px] bg-light-background dark:bg-dark-background w-[33px] h-[33px] rounded-full mx-auto flex justify-center items-center" :style="isDark?'bottom: 36px':''">
         <img class="block dark:hidden" src="./assets/svg/mobile/light-dark-toggle__sun.svg" alt="Icon einer Sonne">
         <img class="hidden dark:block" src="./assets/svg/mobile/light-dark-toggle__moon.svg" alt="Icon eines Halbmondes">
